440314075770aeb0930e58dbe659fba1a7ead5a3
James Champ <james.champ1@yahoo.com>
2017-03-07 20:38:15 -0500

    Created all activities
    Created each activity needed for this application.  Newly created
    activities have not yet been laid out.

---
+++ app/src/main/java/com/jachamp/android/recipebook/views/AddRecipeActivity.java
@@ -0,0 +1,15 @@
+package com.jachamp.android.recipebook.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jachamp.android.recipebook.R;
+
+public class AddRecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_recipe);
+    }
+}
---
+++ app/src/main/java/com/jachamp/android/recipebook/views/IngredientSearchActivity.java
@@ -0,0 +1,15 @@
+package com.jachamp.android.recipebook.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jachamp.android.recipebook.R;
+
+public class IngredientSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingredient_search);
+    }
+}
