29cbb6c47bb546a95a0d0571235feef7492a2f07
James Champ <jameschamp@acm.org>
2017-03-27 17:02:06 -0400

    Added comments to RecipeFragment

--- app/src/main/java/com/jachamp/android/recipebook/ui/RecipeFragment.java
+++ app/src/main/java/com/jachamp/android/recipebook/ui/RecipeFragment.java
@@ -10,7 +10,13 @@ import android.view.ViewGroup;
 import com.jachamp.android.recipebook.R;
 
 /**
- * A simple {@link Fragment} subclass.
+ * Displays a recipe.  Recipes contain a list of ingredients, a list of instructions, and
+ * a picture of the food (if one exists).
+ *
+ * This Fragment is used by {@link DisplayRecipeActivity} and {@link RecipeConfirmationActivity}.
+ *
+ * @author James Champ
+ * @version 1.0, 27 March 2017
  */
 public class RecipeFragment extends Fragment {
 
