60d26a0a1c7758a4fb93acf3e938dd7305080d71
James Champ <jameschamp@acm.org>
2017-03-29 00:08:56 -0400

    Updated RecipeBookContract

--- app/src/main/java/com/jachamp/android/recipebook/data/RecipeBookContract.java
+++ app/src/main/java/com/jachamp/android/recipebook/data/RecipeBookContract.java
@@ -31,12 +31,21 @@ public class RecipeBookContract {
     public static final String PATH_NOTE_LIST = "note_list";
     public static final String PATH_INSTRUCTION_LIST = "instruction_list";
 
+    /**
+     * Prevents class from being instantiated.
+     */
+    private RecipeBookContract() {}
+
     //TODO: Add comments to inner classes
 
     /**
      *
      */
     public static final class RecipeBookEntry implements BaseColumns {
+        public static final Uri RECIPE_BOOK_CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+                .appendPath(PATH_RECIPE_BOOK)
+                .build();
+
         public static final String TABLE_NAME = "recipe_book";
 
         public static final String COLUMN_RECIPE = "recipe";
@@ -47,6 +56,10 @@ public class RecipeBookContract {
      *
      */
     public static final class RecipeEntry implements BaseColumns {
+        public static final Uri RECIPE_ENTRY_CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+                .appendPath(PATH_RECIPE)
+                .build();
+
         public static final String TABLE_NAME = "recipe";
 
         public static final String COLUMN_INGREDIENT_LIST = "ingredient_list";
@@ -66,6 +79,10 @@ public class RecipeBookContract {
      *
      */
     public static final class TagListEntry implements BaseColumns {
+        public static final Uri TAG_LIST_CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+                .appendPath(PATH_TAG_LIST)
+                .build();
+
         public static final String TABLE_NAME = "tag_list";
 
         public static final String COLUMN_TAG = "tag";
@@ -75,17 +92,25 @@ public class RecipeBookContract {
      *
      */
     public static final class IngredientListEntry implements BaseColumns {
+        public static final Uri INGREDIENT_LIST_CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+                .appendPath(PATH_INGREDIENT_LIST)
+                .build();
+
         public static final String TABLE_NAME = "ingredient_list";
 
         public static final String COLUMN_INGREDIENT = "ingredient";
+        public static final String COLUMN_DESCRIPTION = "description";
         public static final String COLUMN_AMOUNT = "amount";
-//        public static final String COLUMN_MEASURE = "measure";
     }
 
     /**
      *
      */
     public static final class NoteListEntry implements  BaseColumns {
+        public static final Uri NOTE_LIST_CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+                .appendPath(PATH_NOTE_LIST)
+                .build();
+
         public static final String TABLE_NAME = "note_list";
 
         public static final String COLUMN_SUMMARY = "summary";
@@ -99,6 +124,10 @@ public class RecipeBookContract {
      *
      */
     public static final class IntructionListEntry implements BaseColumns {
+        public static final Uri INSTRUCTION_LIST_CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+                .appendPath(PATH_INSTRUCTION_LIST)
+                .build();
+
         public static final String TABLE_NAME = "instruction_list";
 
         public static final String COLUMN_INSTRUCTION = "instruction";
